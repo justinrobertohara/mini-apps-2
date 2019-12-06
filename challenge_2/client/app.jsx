47193c3dfca0b89feb651ebbb038a6ee05aba5e1
Justin O'Hara <justinrobertohara@gmail.com>
2019-12-05 20:04:34 -0800

    bmr for bitcoin app

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -13,43 +13,46 @@ export default class App extends React.Component {
     axios
       .get('https://api.coindesk.com/v1/bpi/historical/close.json')
       .then(response => {
-        console.log('getting json object jack from axios', response.data);
+        console.log('getting json object jack from axios', response.data.bpi);
+
+        let keys = Object.keys(response.data.bpi);
+        let values = Object.values(response.data.bpi);
+
+        var ctx = 'myChart';
+
+        new Chart(ctx, {
+          type: 'line',
+          data: {
+            //Bring in data
+            labels: keys,
+            datasets: [
+              {
+                label: 'Bitcoin Value in USD',
+                data: values
+              }
+            ]
+          },
+          options: {
+            //Customize chart options
+          }
+        });
       })
       .catch(err => {
         console.log(err);
       });
-
-    var ctx = 'myChart';
-
-    new Chart(ctx, {
-      type: 'line',
-      data: {
-        //Bring in data
-        labels: ['Jan', 'Feb', 'March'],
-        datasets: [
-          {
-            label: 'Sales',
-            data: [86, 67, 91]
-          }
-        ]
-      },
-      options: {
-        //Customize chart options
-      }
-    });
   }
 
   render() {
     return (
       <div>
-        <h1>Hello From Bitcoin Tracker App</h1>
-
+        <h1>Bitcoin Tracker App </h1>
         <canvas
           id="myChart"
           ref={this.chartRef}
           width="400"
           height="400"
         ></canvas>
+        <p>"Powered by CoinDesk"</p>
       </div>
     );
   }
